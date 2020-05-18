f0d061cea1fcfac305b88ef0c35ceaaa0a840505
samuel-24276 <smartmrcs@gmail.com>
2020-05-18 21:22:31 +0800

    实现分类爬取图书

--- WebCrawler/BOOK/BOOK/spiders/book.py
+++ WebCrawler/BOOK/BOOK/spiders/book.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+import requests
 
 
 class BookSpider(scrapy.Spider):
@@ -9,7 +11,7 @@ class BookSpider(scrapy.Spider):
     start_urls = ['https://book.jd.com/booksort.html']
 
     def parse(self, response):
-        dt_list = response.xpath('//*[@id="booksort"]/div[2]/dl/dt[1]')
+        dt_list = response.xpath('//*[@id="booksort"]/div[2]/dl/dt')  # 52个大分类
         # 遍历52个大分类
         for dt in dt_list:
             item = {}
@@ -17,33 +19,40 @@ class BookSpider(scrapy.Spider):
             item['category'] = dt.xpath('./a/text()').extract_first()
             # 根据大分类取小分类
             em_list = dt.xpath('./following-sibling:: *[1]/em')
-            for em in em_list[:1]:
+            for em in em_list[:2]:  # 每个大分类取两个小分类
                 item['small_category'] = em.xpath('./a/text()').extract_first()
+                print('*'*100)
+                print(item)
                 # 小分类链接需要拼接
-                samll_link = 'http:' + em.xpath('./a/@href').extract_first()
-                # print(item)
-                # 发送请求获取小分类里的图书列表,调用parse_book()函数
-                yield scrapy.Request(samll_link, callback=self.parse_book, meta={'book': item})
+                content = em.xpath('./a/@href').extract_first()
+                cont = re.sub(r"[a-z|\:|\.|/]", "", content)
+                index = 'https://list.jd.com/list.html?cat=' + cont + '&page='                
+                small_link = re.sub(r'-', '%2C', index)
+                # temp = requests.get(small_link)
+                # page = int(temp.xpath('//*[@id="J_bottomPage"]/span[2]/em[1]/b'))
+                s = -26
+                for i in range(1):
+                    s = s * 1 + 26
+                    link = small_link + str(i) + '&s=' + str(s) + '&click=0'
+                    print(link)
+                    # 发送请求获取小分类里的图书列表,调用parse_book()函数
+                    yield scrapy.Request(link, callback=self.parse_book, meta={'book': item})
 
     def parse_book(self, response):
         # 取字典
         item = response.meta.get('book')
-        # url = response.
-        print('*'*100)
-        # print(response)
-        print('*'*100)
-        # 解析所有数据
         list_book = response.xpath('//*[@id="J_goodsList"]/ul/li[@ware-type!="0"]/div')
-        # 遍历解析60本书的数据
+        # print(list_book)
+        # 遍历解析52本书的数据,书的数量在不同的日期是变化的，为了反爬
         for book in list_book:
             # 书名，用相对路径./解析
             item['name'] = book.xpath('.//div[@class="p-name"]/a/em/text()').extract_first().strip()
             # 作者
-            item['author'] = book.xpath('.//div[4]/span[1]/a/text()').extract_first().strip()
-            # 出版社 有的书有两个出版社，取第一个
-            item['store'] = book.xpath('.//div[4]/span[2]/a[1]/text()').extract_first().strip()
+            item['author'] = book.xpath('.//div[4]/span[1]/a/text()').extract_first()
+            # 出版社 有的书有两个出版社，取第一个，有的没写作者，只有一个span
+            item['store'] = book.xpath('.//div[4]/span[2]/a[1]/text()').extract_first()
             # 价格
-            item['price'] = book.xpath('.//div[@class="p-price"]/strong/i/text()').extract_first().strip()
+            item['price'] = book.xpath('.//div[@class="p-price"]/strong/i/text()').extract_first()
             # 默认图片
-            item['default_img'] = 'http:' + book.xpath('.//div[@class="p-img"]/a//img/@src').extract_first().strip()
+            item['default_img'] = 'http:' + book.xpath('.//div[@class="p-img"]/a//img/@src').extract_first()
             yield item
--- WebCrawler/jdbook/book/models.py
+++ WebCrawler/jdbook/book/models.py
@@ -9,9 +9,9 @@ class BookInfo(models.Model):
     category = models.CharField(max_length=50, verbose_name='大类', default='大类')
     small_category = models.CharField(max_length=50, verbose_name='小类', default='小类')
     name = models.CharField(max_length=200, default="无", verbose_name="书名")
-    author = models.CharField(max_length=50, default="无", verbose_name="作者")
-    store = models.CharField(max_length=100, default="无", verbose_name="出版社")
-    price = models.DecimalField(decimal_places=2, max_digits=10, default="0.00", verbose_name="价格")
+    author = models.CharField(max_length=50, default="无", verbose_name="作者", null=True)
+    store = models.CharField(max_length=100, default="无", verbose_name="出版社", null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10, default="0.00", verbose_name="价格", null=True)
     default_image = models.ImageField(max_length=200, null=True, verbose_name="图片")
 
     class Meta:  # 别名
